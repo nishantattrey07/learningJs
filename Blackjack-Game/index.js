e923415758c09a5f381046d01313b8506e517a87
nishantattrey07 <nishantattrey07@gmail.com>
2023-06-14 15:47:52 +0530

    added Blackjack game

---
+++ Blackjack-Game/index.js
@@ -0,0 +1,45 @@
+// Using this array to randomly select the cards for Player and Dealer
+const suits = ["Heart", "Diamond", "Club", "Spade"];
+const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
+let playerhand = document.getElementById("player-hand");
+let dealerhand = document.getElementById("dealer-hand");
+let hitbtn = document.getElementById("hit-btn");
+
+
+// This function will be called to select the suit from suits array
+function getRandomSuit(suits) { 
+    const randomIndex = Math.floor(Math.random() * suits.length);
+    return suits[randomIndex];
+}
+
+// This function will be called to select the card from cards array
+function getRandomRank(rank) { 
+    const randomIndex = Math.floor(Math.random() * rank.length);
+    return rank[randomIndex];
+}
+
+
+
+function updatePlayerCard(cards) { 
+    playerhand.textContent = "";
+    cards.forEach( card => {
+        const cardElement = createCard(card.suit, card.rank);
+        playerhand.appendChild(cardElement);
+    });
+}
+
+function createCard(suits, rank) { 
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.textContent = `${rank} of ${suits}`;
+    return cardElement;
+}
+const playerCards = [
+  { suit: "Hearts", rank: "Ace" },
+  { suit: "Spades", rank: "7" }
+];
+
+hitbtn.onclick = () => {
+   
+    updatePlayerCard(playerCards);
+}
