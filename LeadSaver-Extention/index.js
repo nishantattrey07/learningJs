8ec628863b36d6c2e70dc5b7e90d8bdb8b300f95
nishantattrey07 <nishantattrey07@gmail.com>
2023-06-19 21:19:43 +0530

    added localstorage function

--- LeadSaver-Extention/index.js
+++ LeadSaver-Extention/index.js
@@ -18,7 +18,7 @@ if (localStorage.getItem('nameArray')) {
 }
 renderLink();
 function renderLink() {
-    linkList.textContent = "";
+    linkList.innerHTML = "";
     for (let i = 0; i < linkArray.length; i++) {
         const linkElement = document.createElement("a");
         const listItem = document.createElement("li");
@@ -30,6 +30,65 @@ function renderLink() {
     }
 }
 
+function saveTab() { 
+     if (typeof chrome !== "undefined" && chrome.tabs) {
+  // Chrome browser
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currentTab = tabs[0];
+    var link = currentTab.url;
+      var name = currentTab.title;
+            if (link !== "") {
+            if (name !== "") {
+            linkArray.push(link);
+            nameArray.push(name);
+            } else {
+            linkArray.push(link);
+            nameArray.push(link);
+            }
+            inputField.value = ""; // Clear the input fields after adding the link
+            nameField.value = "";
+        
+            // Save the updated arrays to LocalStorage
+            localStorage.setItem('linkArray', JSON.stringify(linkArray));
+            localStorage.setItem('nameArray', JSON.stringify(nameArray));
+        
+            renderLink();
+        } else {
+            alert("Please enter a link");
+        }
+  });
+} else if (typeof browser !== "undefined" && browser.tabs) {
+  // Firefox browser
+  browser.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+    var currentTab = tabs[0];
+    var link = currentTab.url;
+      var name = currentTab.title;
+              if (link !== "") {
+            if (name !== "") {
+            linkArray.push(link);
+            nameArray.push(name);
+            } else {
+            linkArray.push(link);
+            nameArray.push(link);
+            }
+            inputField.value = ""; // Clear the input fields after adding the link
+            nameField.value = "";
+        
+            // Save the updated arrays to LocalStorage
+            localStorage.setItem('linkArray', JSON.stringify(linkArray));
+            localStorage.setItem('nameArray', JSON.stringify(nameArray));
+        
+            renderLink();
+        } else {
+            alert("Please enter a link");
+        }
+
+  });
+} else {
+  console.log("Unsupported browser");
+}
+
+};
     function saveLink() {
     const link = inputField.value.trim();
     const name = nameField.value.trim();
@@ -59,10 +118,14 @@ saveLinkBtn.onclick = () => {
     saveLink();
 };
 
+saveTabBtn.onclick = () => { 
+    saveTab();
+};
+
 deleteBtn.onclick = () => {
     linkArray.length = 0;
     nameArray.length = 0;
-    linkList.textContent = "";
+    linkList.innerHTML = "";
 
     // Clear the arrays from LocalStorage
     localStorage.removeItem('linkArray');
